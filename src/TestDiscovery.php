57d0e57b692cac1fe287f5c5f7836f725d800d3a
mondrake <mondrake.org@gmail.com>
2021-04-05 12:16:14 +0200

    32

--- src/TestDiscovery.php
+++ src/TestDiscovery.php
@@ -74,10 +74,7 @@ class TestDiscovery extends CoreTestDiscovery {
       $parser = new StaticReflectionParser($classname, $finder, TRUE);
       try {
         $info = static::getTestInfo($classname, $parser->getDocComment());
-  dump($info);
         $info['name'] = preg_replace('/Drupal.*Tests/', '...', $info['name']);
-  dump($info);
-exit();
         $info['filename'] = $pathname;
       }
       catch (MissingGroupException $e) {
