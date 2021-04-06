ee00c5cb51642601aade05abb56f7be26efb09ac
mondrake <mondrake.org@gmail.com>
2021-04-06 14:48:52 +0200

    73

--- src/ExecManager.php
+++ src/ExecManager.php
@@ -100,87 +100,9 @@ class ExecManager {
   /**
    * {@inheritdoc}
    */
-  public function checkPath(string $path, string $package = NULL): array {
-    $status = [
-      'output' => '',
-      'errors' => [],
-    ];
-
-    // Execute gm or convert based on settings.
-    $package = $package ?: $this->getPackage();
-    $binary = $package === 'imagemagick' ? 'convert' : 'gm';
-    $executable = $this->getExecutable($binary, $path);
-
-    // If a path is given, we check whether the binary exists and can be
-    // invoked.
-    if (!empty($path)) {
-      // Check whether the given file exists.
-      if (!is_file($executable)) {
-        $status['errors'][] = $this->t('The @suite executable %file does not exist.', ['@suite' => $this->getPackageLabel($package), '%file' => $executable]);
-      }
-      // If it exists, check whether we can execute it.
-      elseif (!is_executable($executable)) {
-        $status['errors'][] = $this->t('The @suite file %file is not executable.', ['@suite' => $this->getPackageLabel($package), '%file' => $executable]);
-      }
-    }
-
-    // In case of errors, check for open_basedir restrictions.
-    if ($status['errors'] && ($open_basedir = ini_get('open_basedir'))) {
-      $status['errors'][] = $this->t('The PHP <a href=":php-url">open_basedir</a> security restriction is set to %open-basedir, which may prevent to locate the @suite executable.', [
-        '@suite' => $this->getPackageLabel($package),
-        '%open-basedir' => $open_basedir,
-        ':php-url' => 'http://php.net/manual/en/ini.core.php#ini.open-basedir',
-      ]);
-    }
-
-    // Unless we had errors so far, try to invoke convert.
-    if (!$status['errors']) {
-      $error = NULL;
-      $this->runOsShell($executable, '-version', $package, $status['output'], $error);
-      if ($error !== '') {
-        $status['errors'][] = $error;
-      }
-    }
-
-    return $status;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function execute(string $command, array $arguments, string &$output = NULL, string &$error = NULL, string $path = NULL): bool {
     $cmd = $this->getExecutable($command, $path);
-    $return_code = $this->runOsShell($cmd, $arguments, $output, $error);
-    if ($return_code !== FALSE) {
-      // If the executable returned a non-zero code, log to the watchdog.
-      if ($return_code != 0) {
-        if ($error === '') {
-          $this->logger->warning("@suite returned with code @code [command: @command @cmdline]", [
-            '@suite' => $command,
-            '@code' => $return_code,
-            '@command' => $cmd,
-            '@cmdline' => $cmdline,
-          ]);
-        }
-        else {
-          // Log $error with context information.
-          $this->logger->error("@suite error @code: @error [command: @command @cmdline]", [
-            '@suite' => $command,
-            '@code' => $return_code,
-            '@error' => $error,
-            '@command' => $cmd,
-            '@cmdline' => $cmdline,
-          ]);
-        }
-        // Executable exited with an error code, return FALSE.
-        return FALSE;
-      }
-
-      // The shell command was executed successfully.
-      return TRUE;
-    }
-    // The shell command could not be executed.
-    return FALSE;
+    return $this->runOsShell($cmd, $arguments, $output, $error);
   }
 
   /**
@@ -207,34 +129,6 @@ class ExecManager {
     $execution_time = Timer::stop('tester:runOsShell')['time'];
 
     return $return_code;
-  }
-
-  /**
-   * Logs a debug message, and shows it on the screen for authorized users.
-   *
-   * @param string $message
-   *   The debug message.
-   * @param string[] $context
-   *   Context information.
-   */
-  public function debugMessage(string $message, array $context) {
-    $this->logger->debug($message, $context);
-    if ($this->currentUser->hasPermission('administer site configuration')) {
-      // Strips raw text longer than 10 lines to optimize displaying.
-      if (isset($context['@raw'])) {
-        $raw = explode("\n", $context['@raw']);
-        if (count($raw) > 10) {
-          $tmp = [];
-          for ($i = 0; $i < 9; $i++) {
-            $tmp[] = $raw[$i];
-          }
-          $tmp[] = (string) $this->t('[Further text stripped. The watchdog log has the full text.]');
-          $context['@raw'] = implode("\n", $tmp);
-        }
-      }
-      // @codingStandardsIgnoreLine
-      $this->messenger->addMessage($this->t($message, $context), 'status', TRUE);
-    }
   }
 
   /**
