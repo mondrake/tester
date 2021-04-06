69c7c1abd803dbde4584407bcaea364ee410ad43
mondrake <mondrake.org@gmail.com>
2021-04-06 08:04:06 +0200

    64

--- src/Form/TesterResultsForm.php
+++ src/Form/TesterResultsForm.php
@@ -124,6 +124,7 @@ class TesterResultsForm extends FormBase {
       $this->messenger()->addError($this->t('No test results to display.'));
       return $this->redirect('tester.test_form');
     }
+dump($test_id);exit();
 
     // Load all classes and include CSS.
     $form['#attached']['library'][] = 'tester/tester';
