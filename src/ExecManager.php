c049388d9b1a47f6bd18b67d86cdfcf259c8d8f8
mondrake <mondrake.org@gmail.com>
2021-04-17 11:44:48 +0200

    432

--- src/ExecManager.php
+++ src/ExecManager.php
@@ -117,6 +117,7 @@ class ExecManager {
     $process_environment_variables = [
       'SIMPLETEST_DB' => Database::getConnectionInfoAsUrl(),
       'SIMPLETEST_BASE_URL' => "http://192.168.1.2/d91",
+      'SYMFONY_DEPRECATIONS_HELPER' => 'disabled',
     ];
 
     Timer::start('tester:runOsShell');
